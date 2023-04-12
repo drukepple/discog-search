1dfc36359e35f84fce8d4b301511038ffd9b0486
Dru Kepple <dru@fittedrunning.com>
2023-04-12 12:12:25 -0600

    Refactor search form to export own state but rely on router for navigation and display

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,4 +1,5 @@
-import { FormEventHandler, useState } from "react";
+import { useRouter } from "next/router";
+import { FormEvent, useEffect, useState } from "react";
 
 const styles = {
   form: {
@@ -6,23 +7,42 @@ const styles = {
     alignItems: 'center',
   }
 }
+
+export const useSearchState = () => {
+  const router = useRouter();
+  const seller = router.query.seller ?? '';
+  const [sellerSlug, setSellerSlug] = useState(seller);
+  const [noCache, setNoCache] = useState(false);
+
+  useEffect(() => {
+    if (router.query.seller !== '') {
+      setSellerSlug(router.query.seller as string);
+    }
+  }, [router.query.seller]);
+
+  return {
+    sellerSlug, setSellerSlug,
+    noCache, setNoCache,
+  }
+}
+
 type SearchFormProps = {
   onSearch: (slug: string, noCache: boolean) => void;
 }
 export default function SearchForm({onSearch}:SearchFormProps) {
-  const [sellerSlug, setSellerSlug] = useState('');
-  const [noCache, setNoCache] = useState(false);
+  const router = useRouter();
+  const {sellerSlug, setSellerSlug, noCache, setNoCache} = useSearchState();
 
-  const handleSubmit = (e) => {
-    e?.preventDefault();
-    onSearch(sellerSlug, noCache)
+  const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push('/?seller=' + sellerSlug, undefined, {shallow: true});
   }
 
   return <form style={styles.form} onSubmit={handleSubmit}>
     <label>Enter the seller slug:
       <input id="socket-id" name="socket_id" type="hidden" />
+      <input type="text" id="seller-slug-input" value={sellerSlug ?? ''} onChange={(e) => setSellerSlug(e.target.value)}  />
     </label>
-    <input type="text" id="seller-slug-input" value={sellerSlug} onChange={(e) => setSellerSlug(e.target.value)}  />
     <label>
       <input type="checkbox" name="no_cache" checked={noCache} onChange={e => setNoCache(e.target.checked)} />
       No cache
