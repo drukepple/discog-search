75e894073329e87ed707c4020884578b2cbc8c73
Dru Kepple <dru@fittedrunning.com>
2023-04-12 12:11:10 -0600

    Clean up logs

--- src/services/search.ts
+++ src/services/search.ts
@@ -4,7 +4,7 @@ export default function search(slug: string, noCache: boolean):Promise<SearchRes
   const params = new URLSearchParams();
   params.append('seller', slug);
   params.append('no_cache', noCache.toString());
-  console.log(params.toString());
+  // console.log(params.toString());
   return fetch('/api/search?' + params.toString())
     .then(r => r.json())
     .catch(e => console.error(e));
